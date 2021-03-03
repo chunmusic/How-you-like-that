036e42e2310b250da4cdf4ac9a74bf96353d7f28
chunmusic <chun_ps@hotmail.com>
2021-03-03 19:00:59 +0900

    update code

--- main.py
+++ main.py
@@ -1,35 +1,83 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import cv2
 import streamlit as st
 import footer
 import os, urllib, cv2
-
+from PIL import Image
 
 def main():
     
-    st.title("Object Detection")
+    st.title("HOW YOU LIKE THAT!!!")
     st.sidebar.markdown("# Model")
     
     readme_text = st.markdown(get_file_content_as_string("instructions.md"))
 
-    
-    # for filename in EXTERNAL_DEPENDENCIES.keys():
-    #     download_file(filename)
+    uploaded_file = st.file_uploader("Upload Image")
 
-    # detection()
+
+    if uploaded_file is not None:
+
+        image = Image.open(uploaded_file)
+        img_array = np.array(image)
+        img_cv = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+        
+        for filename in EXTERNAL_DEPENDENCIES.keys():
+            download_file(filename)
+
+        detection(img_cv)
+
+
 
 @st.cache(show_spinner=False)
 def get_file_content_as_string(path):
-    url = 'https://raw.githubusercontent.com/streamlit/demo-self-driving/master/' + path
+    url = 'https://raw.githubusercontent.com/chunmusic/How-you-like-that/master/' + path
     response = urllib.request.urlopen(url)
-    print(response.read().decode("utf-8"))
     return response.read().decode("utf-8")
 
-def detection():
 
-    img_to_detect = cv2.imread("test_img/bp0.jpg")
+# This file downloader demonstrates Streamlit animation.
+def download_file(file_path):
+    # Don't download the file twice. (If possible, verify the download using the file length.)
+    if os.path.exists(file_path):
+        if "size" not in EXTERNAL_DEPENDENCIES[file_path]:
+            return
+        elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
+            return
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % file_path)
+        progress_bar = st.progress(0)
+        with open(file_path, "wb") as output_file:
+            with urllib.request.urlopen(EXTERNAL_DEPENDENCIES[file_path]["url"]) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
+                        (file_path, counter / MEGABYTES, length / MEGABYTES))
+                    progress_bar.progress(min(counter / length, 1.0))
+
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
+def detection(image):
+
+    img_to_detect = image
     img_height = img_to_detect.shape[0]
     img_width = img_to_detect.shape[1]
 
@@ -57,9 +105,6 @@ def detection():
     boxes_list = []
     confidences_list = []
 
-
-
-
     for object_detection_layer in obj_detection_layers:
         # loop over the detections
         for object_detection in object_detection_layer:
@@ -71,7 +116,7 @@ def detection():
             prediction_confidence = all_scores[predicted_class_id]
         
             # take only predictions with confidence more than 20%
-            if prediction_confidence > 0.20:
+            if prediction_confidence > 0.90:
                 #get the predicted label
                 predicted_class_label = class_labels[predicted_class_id]
                 #obtain the bounding box co-oridnates for actual image from resized image size
@@ -79,7 +124,6 @@ def detection():
                 (box_center_x_pt, box_center_y_pt, box_width, box_height) = bounding_box.astype("int")
                 start_x_pt = int(box_center_x_pt - (box_width / 2))
                 start_y_pt = int(box_center_y_pt - (box_height / 2))
-                
                 
                 class_ids_list.append(predicted_class_id)
                 confidences_list.append(float(prediction_confidence))
@@ -115,22 +159,32 @@ def detection():
         
         # print the prediction in console
         predicted_class_label = "{}: {:.2f}%".format(predicted_class_label, prediction_confidence * 100)
-        print("predicted object {}".format(predicted_class_label))
+        # print("predicted object {}".format(predicted_class_label))
         
+
+
         # draw rectangle and text in the image
-        cv2.rectangle(img_to_detect, (start_x_pt, start_y_pt), (end_x_pt, end_y_pt), box_color, 1)
-        cv2.putText(img_to_detect, predicted_class_label, (start_x_pt, start_y_pt-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
+        cv2.rectangle(img_to_detect, (start_x_pt, start_y_pt), (end_x_pt, end_y_pt), box_color, 4)
+        cv2.putText(img_to_detect, predicted_class_label, (start_x_pt, start_y_pt-5), cv2.FONT_HERSHEY_SIMPLEX, 1, box_color, 4)
 
+    # image_output = Image.open(img_to_detect)
 
+    # image = Image.open('test_img/bp0.jpg')
+
+    # st.image(img_out, caption='Sunrise by the mountains', use_column_width=True)
+
+    img_to_detect = cv2.cvtColor(img_to_detect,cv2.COLOR_BGR2RGB)
+
+    st.image(img_to_detect, use_column_width=True)
 
 EXTERNAL_DEPENDENCIES = {
     "bp_yolov4_best.weights": {
         "url": "https://github.com/chunmusic/How-you-like-that/raw/master/bp_yolov4_best.weights",
-        "size": 248007048
+        "size": 256080600
     },
     "bp_yolov4.cfg": {
         "url": "https://raw.githubusercontent.com/chunmusic/How-you-like-that/master/bp_yolov4.cfg",
-        "size": 8342
+        "size": 12223
     }
 }
 
